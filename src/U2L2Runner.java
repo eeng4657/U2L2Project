ab3a0ec2ee8799fc88232c04b8395ae35e68b389
eeng4657 <eeng4657@bths.edu>
2022-09-23 12:26:33 -0400

    Initial commit

---
+++ src/U2L2Runner.java
@@ -0,0 +1,10 @@
+public class U2L2Runner {
+    public static void main(String[] args){
+        Rectangle rect1 = new Rectangle(150, (200-75));
+        Rectangle rect2 = new Rectangle(100+25);
+        Rectangle rect3 = new Rectangle(100,(50+75));
+        rect1.printRectangleInfo();
+        rect2.printRectangleInfo();
+        rect3.printRectangleInfo();
+    }
+}
